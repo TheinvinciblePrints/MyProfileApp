e2afce1ec99f2c340d98ea1b5f5ab2ae9e8cde18
EMMANY7 <nic.promise@yahoo.com>
2018-05-21 14:31:39 +0800

    Initial commit

---
+++ app/src/main/java/com/example/emmabuthen/myprofileapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.emmabuthen.myprofileapp
+
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        
+    }
+}
